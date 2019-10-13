f4b1c8dad5047b381b2ac0c42c799eaadbbd30ce
Levi Giraud <giraudlevi@gmail.com>
2019-10-13 15:28:25 -0600

    added functionality to capture user inputs and store them locally

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 var hours = document.querySelectorAll("textarea");
+var storedEvents = JSON.parse(localStorage.getItem("userEvents"));
+var allEvents = [];
+var event = "";
+var saveButtons = document.querySelectorAll(".saveBtn");
 
 function timeCheck() {
     for (var i = 0; i < hours.length; i++) {
-        console.log(hours[i].dataset.value,",",moment().format('HH'));
+        // console.log(hours[i].dataset.value,",",moment().format('HH'));
         if (hours[i].dataset.value < moment().format('HH')) {
-            hours[i].textContent = "It's past time!";
             hours[i].style.background = 'lightgray';
         }else if (hours[i].dataset.value === moment().format('HH')) {
-            hours[i].textContent = "You're on time!";
             hours[i].style.background = 'red';
         }else{
-            hours[i].textContent = "It's coming up soon!";
             hours[i].style.background = 'lightgreen';
         }
     }
 }
+
+function saveEvents () {
+    for (var i = 0; i < hours.length; i++) {
+        var userEvents = {
+            hour: hours[i].dataset.value,
+            eventText: hours[i].value
+        };
+        allEvents.push(userEvents);
+        
+        localStorage.setItem("userEvents", JSON.stringify(allEvents))
+    }
+};
+
+
